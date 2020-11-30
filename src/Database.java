46613598c659967cd4ae5dbb7fe955e3fb921515
maxerling <maximilianerling@gmail.com>
2020-11-30 14:37:02 +0100

    added Database class with instance variables

---
+++ src/Database.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Max Erling
+ * Date: 2020-11-30
+ * Copyright: MIT
+ * Class: Java20B
+ */
+public class Database {
+    private ArrayList<UserAccount > users;
+    private ArrayList<Category> categories;
+    private ArrayList<Task> tasks;
+
+
+    
+
+}
