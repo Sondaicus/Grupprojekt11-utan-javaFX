bd4c9d86db36008ede021223e250df7dc102ee7f
Sondaicus <54620152+Sondaicus@users.noreply.github.com>
2020-12-04 22:05:19 +0100

    Add files via upload

---
+++ Subject.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Subject
+{
+	private String
+		subjectName;
+	
+	private ArrayList<Task>
+		tasks;
+	
+	public Subject()
+	{
+		tasks = new ArrayList<Task>();
+		
+	}
+	
+	public void createTask(String taskName, String description)
+	{
+	
+	}
+	
+	public void removeTask(String taskName)
+	{
+	
+	}
+	
+	public void printTasks()
+	{
+	
+	}
+	
+}
