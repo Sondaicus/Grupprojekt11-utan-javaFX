b124821797444a53b4e37b4c333f3e910d59e0b9
Sondaicus <54620152+Sondaicus@users.noreply.github.com>
2020-12-07 14:52:16 +0100

    Uppladdning 1

--- src/fileIO.java
+++ src/fileIO.java
@@ -5,9 +5,10 @@ public interface fileIO
 	abstract void setDatabaseFolders();
 	abstract void readAllUserFiles();
 	abstract String[] getUserFilesFromID(int userID) throws IOException;
-	abstract void removeUser(int userID);
+	abstract void removeUser(int userID) throws IOException;
 	abstract void writeToIndividualUserFile(String userID, String userName, String[] information) throws IOException;
 	abstract void overwriteFile(String fullFilePath, String[] information) throws IOException;
+	abstract void overwriteFile(String fullFilePath, String information) throws IOException;
 	abstract void addToList();
 	abstract void removeFromList();
 }
