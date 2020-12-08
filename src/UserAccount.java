af839a7a551b88cddbe14d51ba826ad7b8809ab6
Donatas Auzelis <donatas963@gmail.com>
2020-12-08 10:40:12 +0100

    Created printCategorizedTasks method

--- src/UserAccount.java
+++ src/UserAccount.java
@@ -1,14 +1,44 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 public class UserAccount extends Account
 {
 	
-	
-	public UserAccount(String username, String password,ArrayList <Subject>
-			subjects)
+
+	public UserAccount(String username, String password,ArrayList <Subject> subjects)
+
 	{
 		super(username, password, 1,subjects);
-		
+	}
+
+	public static void printCategorizedTasks(String category, String userFile) throws IOException {
+
+		System.out.println("--------------");
+
+		BufferedReader reader = new BufferedReader(new FileReader(userFile));
+
+		String[] userList;
+		String line;
+
+		while ((line = reader.readLine()) != null) {
+
+			userList = line.split(",");
+
+			for (String word: userList) {
+
+				if (word.equals(category))
+					System.out.println(line.replace("," + category, ""));
+			}
+		}
+
+		System.out.println("--------------");
+	}
+
+	public static void main(String[] args) throws IOException {
+		printCategorizedTasks("Inköpslista", "Files/Erik.txt");
+		printCategorizedTasks("Hemmasysslor", "Files/Erik.txt");
 	}
 	
 }
