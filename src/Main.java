323106447e74166445789b816541e16afc3b89b7
rost2001 <robin.stempa@yh.nackademin.se>
2020-12-10 08:50:10 +0000

    valmöjligheter main

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,49 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+
+
     public static void main(String[] args) {
-        ArrayList<Account> users = new ArrayList<>();
-        ArrayList<Task> tasks = new ArrayList<>();
-        UserAccount u = new UserAccount("Robin","123",tasks);
-        AdminAccount a = new AdminAccount("p","p",users);
 
-        u.createTask("Handla","Handla ägg och lök");
-        u.createTask("Städa","Städa rummet och köket");
-        u.createTask("Film","Kolla på The Desgin Pattern");
-
-        u.removeTask("film");
-
-        u.printTasks();
+        Scanner sc = new Scanner(System.in);
+        String användarnamn = null;
+        String lösenord = null;
+        String kontoTyp = null;
 
 
+        while (true) {
+            System.out.println("Vill du logga in skriv 1" + "\n" + "vill du skapa konto skriv 2");
+            String answer = sc.next();
+
+            if (answer.equalsIgnoreCase("1")) {
+                System.out.println("Ange användarnam.");
+                användarnamn = sc.next();
+                System.out.println("Ange lösenord.");
+                lösenord = sc.next();
+                break;
+            } else if (answer.equalsIgnoreCase("2")) {
+                System.out.println("Ange användarnam.");
+                användarnamn = sc.next();
+                System.out.println("Ange lösenord.");
+                lösenord = sc.next();
+                System.out.println("För vanlig användare skriv 1" + "\n" + "För admin skriv 2");
+                kontoTyp = sc.next();
+
+                if (kontoTyp.equalsIgnoreCase("1")) {
+                    kontoTyp = "user";
+                    System.out.println("1. lägg till uppgift" + "\n"
+                            + "2. ta bort uppgift" + "\n"
+                            + "3. se alla uppgifter" + "\n"
+                            + "4. logga ut");
+                } else if (kontoTyp.equalsIgnoreCase("2")) {
+                    kontoTyp = "admin";
+                    System.out.println("1. Ta bort ett konto" + "\n"
+                            + "2. Se alla användare" + "\n"
+                            + "3. logga ut");
+                }
+                break;
+            }
+        }
 
     }
 
